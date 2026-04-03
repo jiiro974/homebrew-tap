f27a6655e8e5edb328b7366bfdff2c6fb025de1e
jiiro974 <jiiro@pm.me>
2026-04-03 09:04:25 +0200

    Update formula to v0.3.0

--- Formula/pafw.rb
+++ Formula/pafw.rb
@@ -1,30 +1,28 @@
 class Pafw < Formula
   desc "CLI tools for Palo Alto Networks firewalls"
   homepage "https://github.com/jiiro974/pafw-releases"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.2.0/pafw-darwin-arm64.tar.gz"
-      sha256 "d2147c6a3336dbbfceacf7042a065b6ce61a03d10f847a4d182825f4966130ea"
+      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.3.0/pafw-darwin-arm64.tar.gz"
+      sha256 "13b1484c96c1d40daa72264a346da68d8d29b4ad63aab7c716abdc5626103de0"
     end
-
     on_intel do
-      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.2.0/pafw-darwin-amd64.tar.gz"
-      sha256 "bbbb700f9c03dd4616b7ef8f7fa16be9f1ba2a234e653f6aae033f27f3bfbfc5"
+      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.3.0/pafw-darwin-amd64.tar.gz"
+      sha256 "e05a6eacb34775ab2f0eba3919d61f91e8ff8ed2d710e90805bc698610f280e8"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.2.0/pafw-linux-arm64.tar.gz"
-      sha256 "480bfd9cee25a0dbe595648a203cb137cdded79c73834b4fb834a15f9be87a43"
+      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.3.0/pafw-linux-arm64.tar.gz"
+      sha256 "67d59ee291a8e36e21610d64e1a4b56995f2d40ee4838eb2d391b22366d02a03"
     end
-
     on_intel do
-      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.2.0/pafw-linux-amd64.tar.gz"
-      sha256 "34fd64e25d9dc81eb7259b0a2ad1d4e05104cb5a8eab2f0aaa51feefeb57c414"
+      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.3.0/pafw-linux-amd64.tar.gz"
+      sha256 "203c50e6e88bff20ed90705e8a73415fcd7b0895c5b3825148c591d53d73cf47"
     end
   end
 
@@ -33,6 +31,6 @@ class Pafw < Formula
   end
 
   test do
-    assert_match "v0.2.0", shell_output("#{bin}/pafw --version")
+    assert_match "v0.3.0", shell_output("#{bin}/pafw --version")
   end
 end
