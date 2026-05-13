e3fc6a9ae34455d5d75a19f4c31a5acec0e3b893
jiiro974 <jiiro@pm.me>
2026-05-13 15:06:28 +0400

    pafw 0.3.5: --json on all commands, session filters

--- Formula/pafw.rb
+++ Formula/pafw.rb
@@ -1,28 +1,28 @@
 class Pafw < Formula
   desc "CLI tools for Palo Alto Networks firewalls - Pragma-TIC"
   homepage "https://www.pragma-tic.org"
-  version "0.3.4"
+  version "0.3.5"
   license :cannot_represent
 
   on_macos do
     on_arm do
-      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.3.4/pafw-darwin-arm64.tar.gz"
-      sha256 "c457b7f8616356d676495cd6f21ccd2d9a1448a412308bf71443d338fbd84199"
+      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.3.5/pafw-darwin-arm64.tar.gz"
+      sha256 "f15ae2fc52a2a80102b5b809ffccc0b9093dd5a66bc737d681d1510f8b5003ea"
     end
     on_intel do
-      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.3.4/pafw-darwin-amd64.tar.gz"
-      sha256 "a95fdba6c31b4a62523fc6d07884116bb83ac02cc8edf28b5ca234b828e62591"
+      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.3.5/pafw-darwin-amd64.tar.gz"
+      sha256 "298032ad3c9abed3d98ce41b0d4394f12db13269088db2f4f0c16716a54953f9"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.3.4/pafw-linux-arm64.tar.gz"
-      sha256 "f15511bd410a70c15f64679b8c03c66974d34a32fa4416b6578ae279bc5eda85"
+      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.3.5/pafw-linux-arm64.tar.gz"
+      sha256 "1d43e1a898425468b8e13f8df7adb692bb6c1be4f04c9eaae526dac20456292b"
     end
     on_intel do
-      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.3.4/pafw-linux-amd64.tar.gz"
-      sha256 "dfe03b88dd079b30ff5fadec97e0cc2725c064e4948d0ba70bcc0dc19f21ae3e"
+      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.3.5/pafw-linux-amd64.tar.gz"
+      sha256 "bcd87246d643efc0129cfb61f6ff2201ff3063c5d331e63e69fd59dc41df0a2a"
     end
   end
 
@@ -34,7 +34,7 @@ class Pafw < Formula
   end
 
   test do
-    assert_match "v0.3.4", shell_output("#{bin}/pafw --version")
+    assert_match "v0.3.5", shell_output("#{bin}/pafw --version")
     assert_match "Pragma-TIC", shell_output("#{bin}/pafw --version")
   end
 end
