0b7d1165642babfc54430574a1b149bd7b16f617
jiiro974 <jiiro@pm.me>
2026-05-20 06:47:48 +0400

    pafw v0.4.0

--- Formula/pafw.rb
+++ Formula/pafw.rb
@@ -1,40 +1,41 @@
 class Pafw < Formula
-  desc "CLI tools for Palo Alto Networks firewalls - Pragma-TIC"
-  homepage "https://www.pragma-tic.org"
-  version "0.3.5"
-  license :cannot_represent
+  desc "CLI tools for Palo Alto Networks firewalls"
+  homepage "https://github.com/jiiro974/pafw"
+  version "0.4.0"
+  license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.3.5/pafw-darwin-arm64.tar.gz"
-      sha256 "f15ae2fc52a2a80102b5b809ffccc0b9093dd5a66bc737d681d1510f8b5003ea"
+      url "https://github.com/jiiro974/pafw-releases/releases/download/v#{version}/pafw-darwin-arm64.tar.gz"
+      sha256 "1f3195fe0d018b8e185ae175cb898afc69ab9452c061a3acd4b83aa7dd14a61a" # darwin-arm64
     end
     on_intel do
-      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.3.5/pafw-darwin-amd64.tar.gz"
-      sha256 "298032ad3c9abed3d98ce41b0d4394f12db13269088db2f4f0c16716a54953f9"
+      url "https://github.com/jiiro974/pafw-releases/releases/download/v#{version}/pafw-darwin-amd64.tar.gz"
+      sha256 "b700c6f825bba97dacbea7e08118f6391aff2153d34bfbc38c55d2a17e3472e3" # darwin-amd64
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.3.5/pafw-linux-arm64.tar.gz"
-      sha256 "1d43e1a898425468b8e13f8df7adb692bb6c1be4f04c9eaae526dac20456292b"
+      url "https://github.com/jiiro974/pafw-releases/releases/download/v#{version}/pafw-linux-arm64.tar.gz"
+      sha256 "8d977114b5b6454e959856cb66a4e8c579fd236c0683e6cb541a9d15e15e6379" # linux-arm64
     end
     on_intel do
-      url "https://github.com/jiiro974/pafw-releases/releases/download/v0.3.5/pafw-linux-amd64.tar.gz"
-      sha256 "bcd87246d643efc0129cfb61f6ff2201ff3063c5d331e63e69fd59dc41df0a2a"
+      url "https://github.com/jiiro974/pafw-releases/releases/download/v#{version}/pafw-linux-amd64.tar.gz"
+      sha256 "64c6fe44e916e63e3267f55027c514efb331888faa8596bad139b28faf7e5d50" # linux-amd64
     end
   end
+
+  SYMLINKS = %w[
+    paping patrace paif paroute paarp pasession pafib pacounter pacap pagp palog
+  ].freeze
 
   def install
     bin.install "pafw"
-    %w[paping patrace paif paroute paarp pasession pafib pacounter pacap pagp palog].each do |cmd|
-      bin.install_symlink "pafw" => cmd
-    end
+    SYMLINKS.each { |s| bin.install_symlink "pafw" => s }
   end
 
   test do
-    assert_match "v0.3.5", shell_output("#{bin}/pafw --version")
-    assert_match "Pragma-TIC", shell_output("#{bin}/pafw --version")
+    assert_match version.to_s, shell_output("#{bin}/pafw version")
   end
 end
