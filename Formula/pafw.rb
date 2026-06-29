256aa2ac736fac9774d15acf6b547d653add7ac1
jiiro974 <jiiro@pm.me>
2026-06-29 12:01:23 +0400

    pafw v0.6.2

--- Formula/pafw.rb
+++ Formula/pafw.rb
@@ -7,22 +7,22 @@ class Pafw < Formula
   on_macos do
     on_arm do
       url "https://github.com/jiiro974/pafw-releases/releases/download/v#{version}/pafw-darwin-arm64.tar.gz"
-      sha256 "68aada82039ba1d561608788ce821b976f1e445d41994498e5aa043042a83b7c" # darwin-arm64
+      sha256 "a86c0bfeb39c6a95ce50470795e95b0ba2975cfbba12eb32aca5b02febd493f9" # darwin-arm64
     end
     on_intel do
       url "https://github.com/jiiro974/pafw-releases/releases/download/v#{version}/pafw-darwin-amd64.tar.gz"
-      sha256 "459c77c939d3fffb8e1b91371edbb41e2d1434eb6c8e2d9e2afed9979bdef84c" # darwin-amd64
+      sha256 "0e0254648f32a60f1cf3f091d1e896b5f1f8b278990126bd13cd8e15c895059d" # darwin-amd64
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/jiiro974/pafw-releases/releases/download/v#{version}/pafw-linux-arm64.tar.gz"
-      sha256 "d6a6f603cdc068b514d4c667ef5c0602087974f963076b6ccd899f47c5f9c34a" # linux-arm64
+      sha256 "76950464443a5cebfbaa2c501c762cac4809ef155cf182cf2618d520bf7a4a14" # linux-arm64
     end
     on_intel do
       url "https://github.com/jiiro974/pafw-releases/releases/download/v#{version}/pafw-linux-amd64.tar.gz"
-      sha256 "4d279435d173c656b3dca4676410f4828587c6ecfb25a851682e2510118051fd" # linux-amd64
+      sha256 "c815326c5d455f7eb6cdd8459fd056d2a886cb9205f9ce1b4644de7ef85987cf" # linux-amd64
     end
   end
 
